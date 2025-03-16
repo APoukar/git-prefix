5bedbe7cd073340733e667cb2c2d4e1ad5cb1c6a
Adam Poukar <adam@poukar.net>
2025-03-16 02:01:44 +0100

    Placed command in Git Top Menu

---
+++ GitPrefix/Commands/MyCommand.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Internal.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio;
+
+namespace GitPrefix
+{
+    [Command(PackageIds.AppendGitPrefix)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("GitPrefix", "Button clicked");
+        }
+    }
+}
